1bb5078f1965437e61061c5bee38fc5006249530
Balu Patil <balu.patil@walchandsangli.ac.in>
2026-04-08 00:39:58 +0530

    Successfully implemented the first pphase of backend ,DB connection

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import fileRoutes from "./routes/fileRoutes.js";
+import { errorHandler } from "./middlewares/errorMiddleware.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/uploads", express.static("uploads"));
+
+app.use("/api/files", fileRoutes);
+
+app.use(errorHandler);
+
+export default app;
