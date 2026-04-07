2cea5a99abb390668959e955a33673db1e01d954
Balu Patil <balu.patil@walchandsangli.ac.in>
2026-04-08 00:33:02 +0530

    added FileSchema

---
+++ backend/models/File.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const fileSchema = new mongoose.Schema(
+  {
+    fileName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    fileSize: {
+      type: Number,
+      required: true,
+    },
+    filePath: {
+      type: String,
+      required: true,
+    },
+    fileUrl: {
+      type: String, // will be used later for S3
+    },
+    code: {
+      type: String, // 6-digit access code
+    },
+    expiryDate: {
+      type: Date,
+    },
+    downloadCount: {
+      type: Number,
+      default: 0,
+    },
+    uploadedBy: {
+      type: String, // later: user ID (for auth)
+    },
+  },
+  {
+    timestamps: true, // adds createdAt & updatedAt
+  }
+);
+
+const File = mongoose.model("File", fileSchema);
+
+export default File;
