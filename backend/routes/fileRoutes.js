e12a7e188126de8011b2757a169bee3aa1d33c7e
Balu Patil <balu.patil@walchandsangli.ac.in>
2026-04-08 00:35:33 +0530

    added file upload routes

---
+++ backend/routes/fileRoutes.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { uploadFile, getFiles } from "../controllers/fileController.js";
+import { upload } from "../middlewares/multer.middleware.js";
+
+const router = express.Router();
+
+// middleware used here 👇
+router.post("/upload", upload.single("file"), uploadFile);
+router.get("/files", getFiles);
+
+export default router;
